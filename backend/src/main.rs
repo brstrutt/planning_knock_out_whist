0a1092b277b2daf4d0bd096675ac53437c2e6a36
brstrutt <ben.strutt@hotmail.co.uk>
2025-07-12 23:12:58 +0000

    [backend]add API to allow setting the message that /api/hey returns

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,24 +1,46 @@
-use actix_files::Files;
-use actix_web::{App, HttpServer, get, web};
-use serde::Serialize;
+use std::sync::Mutex;
 
-#[derive(Serialize)]
+use actix_files::Files;
+use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize)]
 struct Message {
     text: String,
 }
 
 #[get("/hey")]
-async fn hey() -> web::Json<Message> {
+async fn get_hey(data: web::Data<AppStateWithMessage>) -> web::Json<Message> {
     web::Json(Message {
-        text: String::from("HeloooOOOooOOO"),
+        text: data.message.lock().unwrap().clone(),
     })
+}
+
+#[post("/hey")]
+async fn post_hey(req_body: web::Json<Message>, data: web::Data<AppStateWithMessage>) -> impl Responder {
+    let mut message = data.message.lock().unwrap();
+    *message = req_body.text.clone();
+    HttpResponse::Ok()
+}
+
+struct AppStateWithMessage {
+    message: Mutex<String>, // <- Mutex is necessary to mutate safely across threads
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
+    let data = web::Data::new(AppStateWithMessage {
+        message: Mutex::new(String::from("no message")),
+    });
+
+    HttpServer::new(move || {
         App::new()
-            .service(web::scope("/api").service(hey))
+            .app_data(data.clone())
+            .service(
+                web::scope("/api")
+                .service(get_hey)
+                .service(post_hey)
+            )
             // Put this last, else it will claim the entire "/" namespace and none of the other services under it will respond
             .service(
                 Files::new("/", "./public")
