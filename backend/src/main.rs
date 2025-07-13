aec2f622f9b0f29439f7988453cc22a30a1996f9
brstrutt <ben.strutt@hotmail.co.uk>
2025-07-13 12:07:53 +0000

    [backend]add basic logging to the webserver

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -2,6 +2,8 @@ use std::sync::Mutex;
 
 use actix_files::Files;
 use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use actix_web::middleware::Logger;
+use env_logger::Env;
 use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize)]
@@ -18,8 +20,9 @@ async fn get_hey(data: web::Data<AppStateWithMessage>) -> web::Json<Message> {
 
 #[post("/hey")]
 async fn post_hey(req_body: web::Json<Message>, data: web::Data<AppStateWithMessage>) -> impl Responder {
-    let mut message = data.message.lock().unwrap();
-    *message = req_body.text.clone();
+    let new_message = req_body.text.clone();
+    let mut current_message = data.message.lock().unwrap();
+    *current_message = new_message;
     HttpResponse::Ok()
 }
 
@@ -29,12 +32,16 @@ struct AppStateWithMessage {
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    env_logger::init_from_env(Env::default().default_filter_or("info"));
+
     let data = web::Data::new(AppStateWithMessage {
         message: Mutex::new(String::from("no message")),
     });
 
     HttpServer::new(move || {
         App::new()
+            .wrap(Logger::default())
+            .wrap(Logger::new("%a %{User-Agent}i"))
             .app_data(data.clone())
             .service(
                 web::scope("/api")
