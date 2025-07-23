04ecd8065d3cd026aeb5318fff01db03173627df
brstrutt <ben.strutt@hotmail.co.uk>
2025-07-23 10:27:41 +0000

    [backend] connect API respond with useful JSON object

--- backend/src/api/session.rs
+++ backend/src/api/session.rs
@@ -1,29 +1,47 @@
-use actix_web::{post, web, HttpResponse, Responder};
-use serde::{Deserialize};
+use actix_web::{post, web};
+use serde::{Deserialize, Serialize};
 
 use crate::state::{AppState, Session};
 
 #[derive(Deserialize)]
-pub struct ConnectData {
+struct ConnectRequest {
     session_uuid: String,
+}
+
+#[derive(Serialize)]
+enum ResponseType {
+    SessionRestored,
+    SessionCreated,
+    TooManySessions
+}
+
+#[derive(Serialize)]
+struct ConnectResponse {
+    session_status: ResponseType
 }
 
 const MAX_SESSIONS: usize = 20;
 
 #[post("/connect")]
-pub async fn post_connect(req_body: web::Json<ConnectData>, data: web::Data<AppState>) -> impl Responder {
+pub async fn post_connect(req_body: web::Json<ConnectRequest>, data: web::Data<AppState>) -> web::Json<ConnectResponse> {
     let session_uuid = req_body.session_uuid.clone();
     let mut current_sessions = data.sessions.lock().unwrap();
 
     let session_already_active = current_sessions.iter().any(|session| session.uuid == session_uuid);
     if session_already_active {
-        return HttpResponse::Ok().body("Already active");
+        return web::Json(ConnectResponse {
+            session_status: ResponseType::SessionRestored,
+        })
     }
 
     if current_sessions.len() >= MAX_SESSIONS {
-        return HttpResponse::Ok().body("Too many sessions");
+        return web::Json(ConnectResponse {
+            session_status: ResponseType::TooManySessions,
+        })
     }
 
     current_sessions.push(Session{uuid: session_uuid});
-    HttpResponse::Ok().body("New session")
+    web::Json(ConnectResponse {
+        session_status: ResponseType::SessionCreated,
+    })
 }
