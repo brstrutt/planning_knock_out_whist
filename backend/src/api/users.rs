c361592b1a34705d4514cfd2fad9642abc0e8ce9
brstrutt <ben.strutt@hotmail.co.uk>
2025-07-27 20:49:15 +0000

    [backend] add outline of intended users get/update/create tests and move list tests to separate module

--- backend/src/api/users.rs
+++ backend/src/api/users.rs
@@ -51,61 +51,85 @@ mod tests {
     use actix_web::{App, test, web};
     use std::sync::Mutex;
 
-    #[actix_web::test]
-    async fn list_empty() {
-        // Setup actix
-        let app_data = web::Data::new(AppState {
-            message: Mutex::new(String::from("")),
-            sessions: Mutex::new(Vec::new()),
-        });
-        let app =
-            test::init_service(App::new().app_data(app_data.clone()).service(super::list)).await;
-        let req = test::TestRequest::get().uri("/users").to_request();
+    use super::*;
 
-        // // Call the API
-        let resp: Vec<super::User> = test::call_and_read_body_json(&app, req).await;
+    mod list {
+        use super::*;
 
-        // Check the response
-        assert_eq!(resp.len(), 0);
+        #[actix_web::test]
+        async fn list_empty() {
+            // Setup actix
+            let app_data = web::Data::new(AppState {
+                message: Mutex::new(String::from("")),
+                sessions: Mutex::new(Vec::new()),
+            });
+            let app =
+                test::init_service(App::new().app_data(app_data.clone()).service(super::list))
+                    .await;
+            let req = test::TestRequest::get().uri("/users").to_request();
+
+            // // Call the API
+            let resp: Vec<super::User> = test::call_and_read_body_json(&app, req).await;
+
+            // Check the response
+            assert_eq!(resp.len(), 0);
+        }
+
+        #[actix_web::test]
+        async fn list() {
+            // Setup actix
+            let app_data = web::Data::new(AppState {
+                message: Mutex::new(String::from("")),
+                sessions: Mutex::new(vec![
+                    Session {
+                        uuid: String::from("1"),
+                        id: 1,
+                        name: Some(String::from("testing")),
+                    },
+                    Session {
+                        uuid: String::from("several"),
+                        id: 2,
+                        name: None,
+                    },
+                    Session {
+                        uuid: String::from("Third"),
+                        id: 3,
+                        name: Some(String::from("manymanymanyword long name here")),
+                    },
+                ]),
+            });
+            let app =
+                test::init_service(App::new().app_data(app_data.clone()).service(super::list))
+                    .await;
+            let req = test::TestRequest::get().uri("/users").to_request();
+
+            // // Call the API
+            let resp: Vec<super::User> = test::call_and_read_body_json(&app, req).await;
+
+            // Check the response
+            assert_eq!(resp.len(), 3);
+            assert_eq!(resp[0].id, "1");
+            assert_eq!(resp[0].name, "testing");
+            assert_eq!(resp[1].id, "2");
+            assert_eq!(resp[1].name, "Unknown User 2");
+            assert_eq!(resp[2].id, "3");
+            assert_eq!(resp[2].name, "manymanymanyword long name here");
+        }
     }
 
-    #[actix_web::test]
-    async fn list() {
-        // Setup actix
-        let app_data = web::Data::new(AppState {
-            message: Mutex::new(String::from("")),
-            sessions: Mutex::new(vec![
-                Session {
-                    uuid: String::from("1"),
-                    id: 1,
-                    name: Some(String::from("testing")),
-                },
-                Session {
-                    uuid: String::from("several"),
-                    id: 2,
-                    name: None,
-                },
-                Session {
-                    uuid: String::from("Third"),
-                    id: 3,
-                    name: Some(String::from("manymanymanyword long name here")),
-                },
-            ]),
-        });
-        let app =
-            test::init_service(App::new().app_data(app_data.clone()).service(super::list)).await;
-        let req = test::TestRequest::get().uri("/users").to_request();
+    // Get
+    // Test a valid user ID produces the correct user data
+    // Test a non-existant user ID produces a 404 error
+    // Test an invalid user Id produces a 404 error
 
-        // // Call the API
-        let resp: Vec<super::User> = test::call_and_read_body_json(&app, req).await;
+    // Create
+    // Test a missing UUID returns a bad_request error
+    // Test a valid UUID returns the expected new user and that user is persisted
 
-        // Check the response
-        assert_eq!(resp.len(), 3);
-        assert_eq!(resp[0].id, "1");
-        assert_eq!(resp[0].name, "testing");
-        assert_eq!(resp[1].id, "2");
-        assert_eq!(resp[1].name, "Unknown User 2");
-        assert_eq!(resp[2].id, "3");
-        assert_eq!(resp[2].name, "manymanymanyword long name here");
-    }
+    // Update
+    // Test a missing UUID returns a bad_request error
+    // Test a UUID that doesn't match the modified user returns a forbidden error
+    // Test modifying a non-existant user returns 404
+    // Test modification of ID throws bad_request
+    // Test modification of Name works fine
 }
